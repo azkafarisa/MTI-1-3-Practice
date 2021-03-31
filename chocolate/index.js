71a7068d751005f2191ee5454aada9986e3417b7
Rizky Ian Indiarto <rizkyian78@gmail.com>
2021-03-31 21:57:28 +0700

    asda

---
+++ chocolate/index.js
@@ -0,0 +1,14 @@
+let result = 0
+
+const Bonus = (cokelat) => { 
+};
+ 
+ const test = (testCase, result) => {
+     if (testCase !== result) return console.log(false);
+     return console.log(true);
+ };
+ 
+ test(Bonus(60), 74);
+//  test(Bonus(100), 124);
+//  test(Bonus(1000), 1249);
+//  test(Bonus(1234), 1539);
